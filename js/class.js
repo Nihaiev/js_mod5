63e4e041575843a7ef7a113c014a490325dd8b9d
Ivan <nigaev.ivan@gmail.com>
2022-07-23 21:42:24 +0300

    Update class.js

--- js/class.js
+++ js/class.js
@@ -9,3 +9,15 @@
  *  - Гетери і сеттери 
  */
 
+class Car1 { 
+    constructor() {
+        console.log('Виконується конструктор');
+    }
+}
+console.dir(Car1)
+const carInstance = new Car1(
+    brand: 'Audi',
+    model: 'Q3',
+    price: 35000,
+);
+console.log(carInstance);
