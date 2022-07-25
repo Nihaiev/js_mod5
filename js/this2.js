fb2eb3f460615c63a0cbc04e5bbd3e16f64108ef
Ivan <nigaev.ivan@gmail.com>
2022-07-25 20:59:38 +0300

    this2

---
+++ js/this2.js
@@ -0,0 +1,30 @@
+//* This
+
+// function foo() {
+//     console.log(this)
+// }
+
+// const obj = {
+//     name: 'Denis',
+//     foo: foo,
+//     info: {
+//         work: "Golf",
+//         getInfo() {
+//             console.log(this);
+//         }
+//     }
+// }
+
+// foo()
+// obj.foo()
+// obj.info.getInfo() // =>
+
+// TODO Example #1
+
+const rectangle = {
+    width: 10,
+    height: 10,
+    getSquare() {
+        
+    }
+}
